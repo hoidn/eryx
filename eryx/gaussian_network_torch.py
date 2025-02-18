fix: Update dtype cast from complex64 to complex128 in hessian assignment

eryx/gaussian_network_torch.py
@@ -99,7 +99,7 @@ class GaussianNetworkModelTorch:
         # Set the diagonal (reference cell)
         for i_asu in range(self.n_asu):
             for i_at in range(self.n_atoms_per_asu):
-                hessian[i_asu, i_at, self.id_cell_ref, i_asu, i_at] = -hessian_diag[i_asu, i_at] - self.gamma[self.id_cell_ref, i_asu, i_asu].to(torch.complex64)
+                hessian[i_asu, i_at, self.id_cell_ref, i_asu, i_at] = -hessian_diag[i_asu, i_at] - self.gamma[self.id_cell_ref, i_asu, i_asu].to(torch.complex128)
             logging.debug(
                 f"[DEBUG compute_hessian] Before diagonal assignment for ASU={i_asu}, atom index={i_at}: "
                 f"hessian_diag = {hessian_diag[i_asu, i_at].item()}, gamma (ref cell) = {self.gamma[self.id_cell_ref, i_asu, i_asu].item()}"